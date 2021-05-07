2b5a6f8b665c561c845296ee5ed02336cdbb40ce
Javier E. Hernández Sánchez <javier.edo.hdez.schez@gmail.com>
2021-05-07 13:25:29 +0200

    añado descripción al ejercicio 5

--- 005.py
+++ 005.py
@@ -2,6 +2,10 @@
 from getpass import getuser
 from datetime import date
 
+# generar un archivo que contenga:
+# linea1: #!/bin/bash
+# linea2: nombre del usuario y fecha de la creación
+
 out_file = "005.out"
 user = getuser()
 today = date.today()
